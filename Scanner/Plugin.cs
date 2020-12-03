0385ffab8b185e58ed177432d1c1645ec12ff438
Thundermaker300 <thundermaker300@gmail.com>
2020-12-02 21:10:01 -0500

    Version bump

--- Scanner/Plugin.cs
+++ Scanner/Plugin.cs
@@ -40,7 +40,7 @@ namespace Scanner
 
         public override string Name => "Scanner";
         public override string Author => "Thunder";
-        public override Version Version => new Version(1, 0, 0);
+        public override Version Version => new Version(1, 0, 1);
         public override Version RequiredExiledVersion => new Version(2, 1, 18);
         public override PluginPriority Priority => PluginPriority.High;
     }
