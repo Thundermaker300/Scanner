77428031d77216c89ec91bfd9f0c2cdbb07041e1
Thunder <thundermaker300@gmail.com>
2023-02-11 12:18:50 -0500

    Add CassieMessage default ctor

--- Scanner/Structures/CassieMessage.cs
+++ Scanner/Structures/CassieMessage.cs
@@ -11,6 +11,11 @@ namespace Scanner.Structures
         public string CassieText { get; set; } = "UNKNOWN";
         public string CaptionText { get; set; } = "UNKNOWN";
 
+        public CassieMessage()
+            : this("UNKNOWN", "UNKNOWN")
+        {
+        }
+
         public CassieMessage(string cassieText, string captionText)
         {
             CassieText = cassieText;
