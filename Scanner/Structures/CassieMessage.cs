2cdbab80fc1f371fcd5183353d1f1c9de078544b
Thunder <thundermaker300@gmail.com>
2023-02-11 12:16:12 -0500

    Update CassieMessage.cs

--- Scanner/Structures/CassieMessage.cs
+++ Scanner/Structures/CassieMessage.cs
@@ -8,8 +8,8 @@ namespace Scanner.Structures
 {
     public class CassieMessage
     {
-        public string CassieText { get; set; } = string.Empty;
-        public string CaptionText { get; set; } = string.Empty;
+        public string CassieText { get; set; } = "UNKNOWN";
+        public string CaptionText { get; set; } = "UNKNOWN";
 
         public CassieMessage(string cassieText, string captionText)
         {
